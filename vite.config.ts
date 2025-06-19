3cde3c9d3bc970af83a2777a6f9b18cf37d8dae9
kolaflash <kolaflash@GAMBIT>
2025-06-20 00:25:24 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,40 @@
-import { defineConfig, type Plugin } from "vite";
+import { defineConfig, type Plugin, type Connect } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import { readdirSync, writeFileSync } from "node:fs";
+import { type ServerResponse, IncomingMessage } from "node:http";
 
-function assetIndexer(): Plugin {
-  const indexName = "index.json";
+function devToolMiddleware(
+  req: IncomingMessage,
+  res: ServerResponse,
+  next: Connect.NextFunction,
+): void {
+  // console.log(req, res);
+  next();
+}
+
+function devTools(): Plugin {
+  const assetIndex = "index.json";
   const assetsDir = "public/assets";
 
   return {
-    name: "asset-indexer",
+    name: "dev-tools",
+    configureServer(server) {
+      server.middlewares.use(devToolMiddleware);
+    },
     buildStart() {
       const assets = readdirSync(assetsDir, {
         withFileTypes: true,
         recursive: true,
       });
       const index = assets
-        .filter((dirent) => dirent.isFile() && dirent.name !== indexName)
+        .filter((dirent) => dirent.isFile() && dirent.name !== assetIndex)
         .map((dirent) => {
           const path = dirent.parentPath.substring(assetsDir.length);
           return `${path}/${dirent.name}`;
         });
 
       writeFileSync(
-        `${assetsDir}/${indexName}`,
+        `${assetsDir}/${assetIndex}`,
         JSON.stringify(index, null, 2),
         "utf8",
       );
@@ -35,5 +48,5 @@ export default defineConfig({
     hmr: false,
   },
   build: { target: "esnext" },
-  plugins: [solidPlugin(), assetIndexer()],
+  plugins: [solidPlugin(), devTools()],
 });
