3605ac8f2753e62c7b6b5f61777fff96a9723e51
kolaflash <kolaflash@GAMBIT>
2025-09-07 13:04:22 +0200

    Improve objectEquals function

--- lib/objectEquals.ts
+++ lib/objectEquals.ts
@@ -1,11 +1,11 @@
 /**
- * Compares the own properties of `a` with the coresponding properties of `b`.
+ * Compares the own properties of `a` with the corresponding properties of `b`.
  * Returns `true` if there are value wise equal.
  * If `b` is an extension of `a`, the extending properties are ignored.
  * The objects must be transferable for comparison.
  * https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Transferable_objects
  */
-export function objectEquals(a: object, b: object): boolean {
+export function objectEquals<T extends object>(a: T, b: T): boolean {
   if (typeof a !== typeof b) {
     return false;
   }
@@ -13,17 +13,18 @@ export function objectEquals(a: object, b: object): boolean {
   let truth = true;
 
   for (const prop in a) {
-    // eslint-disable-next-line no-prototype-builtins
-    if (!a.hasOwnProperty(prop)) {
+    if (!Object.hasOwn(a, prop)) {
       continue;
     }
 
-    // @ts-ignore
-    if (typeof a[prop] === "object") {
-      // @ts-ignore
+    if (
+      a[prop] !== null &&
+      b[prop] !== null &&
+      typeof a[prop] === "object" &&
+      typeof b[prop] === "object"
+    ) {
       truth = truth && objectEquals(a[prop], b[prop]);
     } else {
-      // @ts-ignore
       truth = truth && a[prop] === b[prop];
     }
 
